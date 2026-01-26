ce3fa4c2079ed52b088bf379152d3f3f0e8509a3
Vaishnavi_Saranga <141993937+VAISHNAVISARANGA@users.noreply.github.com>
2026-01-26 19:10:06 +0530

    Delete backup_data.py

--- backup_data.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-# Download the last 30+ years of data
-print("Downloading historical backup...")
-data = yf.download(["SPY", "^VIX"], period="max")
-
-# Save it to your project folder
-data.to_csv("historical_backup.csv")
-print("✅ historical_backup.csv created! Now push this file to GitHub.")
